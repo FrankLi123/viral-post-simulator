2687f8c08ae9c190836ecc68641d2f688403eecd
FrankLi123 <zoli@ucsd.edu>
2025-07-16 22:34:32 +0800

    fix: suppressHydrationWarning error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="antialiased">
+    <html lang="en" suppressHydrationWarning={true}>
+      <body className="antialiased" suppressHydrationWarning={true}>
         {children}
       </body>
     </html>
